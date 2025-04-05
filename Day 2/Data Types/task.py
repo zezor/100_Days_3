6a1ed00c25c019cc618bb612169c265a9a596b4f
zezor <kemma2993@gmail.com>
2025-04-05 15:25:16 -0800

    Initial commit

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,20 @@
 print(len("Hello"))
+# subscripting
+print("Hello"[0])
+
+#string
+print("123" + "456") #concatenation
+
+#integer = whole number
+
+print(123 + 456 )
+
+# Large numbers
+print(123456789)
+
+#Float
+print(3.1457)
+
+# Boolean
+print(True)
+print(False)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,21 @@
-len(12345)
+print(len("12345"))
+
+
+# Checking data type
+print(type("ABC"))
+print(type(123))
+print(type(True))
+print(type(123.4))
+
+# Converting from one data type to another data type
+int()
+float()
+str()
+bool()
+
+print(int("123") + int(456))
+
+# concatenate string and integer
+name_of_user = input("Enter your name") #str
+length_of_name = len(name_of_user) # int
+print("Number of letters in your name: " + str(length_of_name))
